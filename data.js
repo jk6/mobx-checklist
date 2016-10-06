a719f048cc51c14bb197364d9109553c41bf5b48
jk6 <jasonkelly6@gmail.com>
2016-10-05 23:01:12 -0400

    Added external data.js file
    Added external data file, made changes to main.js to retrieve data,
    added some Bootstrap styling.

---
+++ data.js
@@ -0,0 +1,22 @@
+let data = [
+    {
+        "id": "1", "title": "Learn Node"
+    },
+    {
+        "id": "2", "title": "Learn Mobx"
+    },
+    {
+        "id": "3", "title": "Learn React Native"
+    },
+    {
+        "id": "4", "title": "Drink Coffee"
+    },
+    {
+        "id": "5", "title": "Watch Silicon Valley"
+    },
+    {
+        "id": "6", "title": "Learn Express"
+    }
+];
+
+export default data
--- main.js
+++ main.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { observable, computed } from 'mobx';
 import { observer } from 'mobx-react';
+import data from './data';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Todo {
     id = Math.random();
     @observable title;
     @observable finished = false;
+    
     constructor(title) {
         this.title = title;
     }
@@ -24,29 +27,35 @@ class TodoListView extends Component {
     render() {
         return (
             <div>
-            <ul>
-                {this.props.todoList.todos.map(todo => 
-                    <TodoView todo={todo} key={todo.id} />
-                )}
-            </ul>
-            {this.props.todoList.unfinishedTodoCount == 0 ?
-            <span><h3>All tasks are complete!</h3></span>
-            :
-            <span><h3>Tasks left: {this.props.todoList.unfinishedTodoCount}</h3></span>
-            }
-        </div>
+                <div className="row">
+                    <h4 className="col-md-4 col-md-offset-8">Checklist</h4>
+                </div>
+                <div className="col-md-4">
+                    <ul className="list-group">
+                        {this.props.todoList.todos.map(todo => 
+                            <TodoView todo={todo} key={todo.id} />
+                        )}
+                    </ul>
+                    {this.props.todoList.unfinishedTodoCount == 0 ?
+                    <span><h3>All tasks are complete!</h3></span>
+                    :
+                    <span><h3>Tasks left: {this.props.todoList.unfinishedTodoCount}</h3></span>
+                    }
+                </div>
+            </div>
         );
     }
 }
 
-const TodoView = observer(({todo}) => 
-    <li>
+const TodoView = observer(({todo}) =>     
+    <li style={style} className="list-group-item">
         <input
             type="checkbox"
             checked={todo.finished}
             onClick={() => todo.finished = !todo.finished}
-        />{todo.finished ? 
-        		<span><strike>{todo.title}</strike></span>
+            style={{width: 15, height: 15}}
+        />&nbsp;{todo.finished ? 
+        	<span className="text-muted" style={{backgroundColor: '#d3d3d3'}}>{todo.title}</span>
             : 
             <span>{todo.title}</span>
         }
@@ -55,14 +64,10 @@ const TodoView = observer(({todo}) =>
 
 const store = new TodoList();
 
-store.todos.push(
-    new Todo("Get Coffee"),
-    new Todo("Write simpler code"),
-    new Todo("Learn Mobx"),
-    new Todo("Learn React Native")
-);
+data.map((item) => store.todos.push(new Todo(item.title)));
+
+const style = {
+    listStyle: 'none'
+};
 
 ReactDOM.render(<TodoListView todoList={store} />, document.getElementById('mount'));
-
-
-//store.todos[0].finished = true;
